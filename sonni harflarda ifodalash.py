795facb3cfb274df7be514b03db78abc2ae9bc1d
LazizbekTATU <115630143+LazizbekTATU@users.noreply.github.com>
2023-01-03 20:47:05 +0500

    Add files via upload

---
+++ sonni harflarda ifodalash.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#1 mlrd gacha sonlarni harflarda ifodalab bering
+
+n = int(input("n = "))
+bir  = n%10
+on = n%100//10
+yuz = n%1000//100
+ming = n%10000//1000
+onming = n%100000//10000
+yuzming = n%1000000//100000
+million = n%10000000//1000000
+onmillion = n%100000000//10000000
+yuzmillion = n%1000000000//100000000
+
+#yuzmillion
+
+if yuzmillion==1:
+    print("yuz",end = " ")
+if yuzmillion==2:
+    print("ikkiyuz",end = " ")
+if yuzmillion==3:
+    print("uchyuz",end = " ")
+if yuzmillion==4:
+    print("tortyuz",end = " ")
+if yuzmillion==5:
+    print("beshyuz",end = " ")
+if yuzmillion==6:
+    print("oltiyuz",end = " ")
+if yuzmillion==7:
+    print("yettiyuz",end = " ")
+if yuzmillion==8:
+    print("sakkizyuz",end = " ")
+if yuzmillion==9:
+    print("toqqizyuz",end = " ")
+
+#onmillion
+
+if onmillion==1:
+    print("o'n",end = " ")
+if onmillion==2:
+    print("yigirma",end = " ")
+if onmillion==3:
+    print("o'ttiz",end = " ")
+if onmillion==4:
+    print("qirq",end = " ")
+if onmillion==5:
+    print("ellik",end = " ")
+if onmillion==6:
+    print("oltmish",end = " ")
+if onmillion==7:
+    print("yetmish",end = " ")
+if onmillion==8:
+    print("sakson",end = " ")
+if onmillion==9:
+    print("to'qson",end = " ")
+
+#million
+
+if million==1:
+    print("bir",end = " ")
+if million==2:
+    print("ikki",end=" ")
+if million==3:
+    print("uch",end = " ")
+if million==4:
+    print("to'rt",end = " ")
+if million==5:
+    print("besh",end = " ")
+if million==6:
+    print("olti",end = " ")
+if million==7:
+    print("yetti",end = " ")
+if million==8:
+    print("sakkiz",end = " ")
+if million==9:
+    print("to'qqiz",end=" ")
+    
+if yuzmillion or onmillion or million:
+    print("million",end = " ")
+
+    
+#yuzming 
+
+
+if yuzming==1:
+    print("yuz",end = " ")
+if yuzming==2:
+    print("ikkiyuz",end = " ")
+if yuzming==3:
+    print("uchyuz",end = " ")
+if yuzming==4:
+    print("tortyuz",end = " ")
+if yuzming==5:
+    print("beshyuz",end = " ")
+if yuzming==6:
+    print("oltiyuz",end = " ")
+if yuzming==7:
+    print("yettiyuz",end = " ")
+if yuzming==8:
+    print("sakkizyuz",end = " ")
+if yuzming==9:
+    print("toqqizyuz",end = " ")
+
+#onming
+
+if onming==1:
+    print("o'n",end = " ")
+if onming==2:
+    print("yigirma",end = " ")
+if onming==3:
+    print("o'ttiz",end = " ")
+if onming==4:
+    print("qirq",end = " ")
+if onming==5:
+    print("ellik",end = " ")
+if onming==6:
+    print("oltmish",end = " ")
+if onming==7:
+    print("yetmish",end = " ")
+if onming==8:
+    print("sakson",end = " ")
+if onming==9:
+    print("to'qson",end = " ")
+    
+#ming
+
+if ming==1:
+    print("bir",end = " ")
+if ming==2:
+    print("ikki",end=" ")
+if ming==3:
+    print("uch",end = " ")
+if ming==4:
+    print("to'rt",end = " ")
+if ming==5:
+    print("besh",end = " ")
+if ming==6:
+    print("olti",end = " ")
+if ming==7:
+    print("yetti",end = " ")
+if ming==8:
+    print("sakkiz",end = " ")
+if ming==9:
+    print("to'qqiz",end=" ")
+
+if yuzming or onming or ming:
+    print("ming",end = " ")
+
+#yuz
+
+if yuz==1:
+    print("yuz",end = " ")
+if yuz==2:
+    print("ikkiyuz",end = " ")
+if yuz==3:
+    print("uchyuz",end = " ")
+if yuz==4:
+    print("tortyuz",end = " ")
+if yuz==5:
+    print("beshyuz",end = " ")
+if yuz==6:
+    print("oltiyuz",end = " ")
+if yuz==7:
+    print("yettiyuz",end = " ")
+if yuz==8:
+    print("sakkizyuz",end = " ")
+if yuz==9:
+    print("toqqizyuz",end = " ")
+
+#on
+
+if on==1:
+    print("o'n",end = " ")
+if on==2:
+    print("yigirma",end = " ")
+if on==3:
+    print("o'ttiz",end = " ")
+if on==4:
+    print("qirq",end = " ")
+if on==5:
+    print("ellik",end = " ")
+if on==6:
+    print("oltmish",end = " ")
+if on==7:
+    print("yetmish",end = " ")
+if on==8:
+    print("sakson",end = " ")
+if on==9:
+    print("to'qson",end = " ")
+
+#bir
+
+if bir==1:
+    print("bir",end = " ")
+if bir==2:
+    print("ikki",end=" ")
+if bir==3:
+    print("uch",end = " ")
+if bir==4:
+    print("to'rt",end = " ")
+if bir==5:
+    print("besh",end = " ")
+if bir==6:
+    print("olti",end = " ")
+if bir==7:
+    print("yetti",end = " ")
+if bir==8:
+    print("sakkiz",end = " ")
+if bir==9:
+    print("to'qqiz",end=" ")
+
+
+# In[ ]:
+
+
+
+
